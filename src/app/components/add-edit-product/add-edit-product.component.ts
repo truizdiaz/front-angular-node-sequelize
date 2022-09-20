0e2c16bd53a91b3ef92e6a3ef1ab9be5aeff90f0
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-09-20 17:25:11 -0300

    Update product

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
@@ -13,39 +13,78 @@ import { ProductService } from 'src/app/services/product.service';
 export class AddEditProductComponent implements OnInit {
   form: FormGroup;
   loading: boolean = false;
+  id: number;
+  operacion: string = 'Agregar ';
 
   constructor(private fb: FormBuilder,
     private _productService: ProductService,
     private router: Router,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    private aRouter: ActivatedRoute) {
     this.form = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       price: [null, Validators.required],
       stock: [null, Validators.required],
     })
-   }
+    this.id = Number(aRouter.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit(): void {
+
+    if (this.id != 0) {
+      // Es editar
+      this.operacion = 'Editar ';
+      this.getProduct(this.id);
+    }
+  }
+
+  getProduct(id: number) {
+    this.loading = true;
+    this._productService.getProduct(id).subscribe((data: Product) => {
+      this.loading = false;
+      this.form.setValue({
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock
+      })
+    })
   }
 
   addProduct() {
-   /*  console.log(this.form.value.name);
-    console.log(this.form.get('name')?.value); */
+    /*  console.log(this.form.value.name);
+     console.log(this.form.get('name')?.value); */
 
     const product: Product = {
       name: this.form.value.name,
       description: this.form.value.description,
       price: this.form.value.price,
       stock: this.form.value.stock
-    } 
-
+    }
     this.loading = true;
-    this._productService.saveProduct(product).subscribe(() => {
-      this.loading = false;
-      this.toastr.success(`El producto ${product.name} fue registrado con exito`, 'Producto registrado');
-      this.router.navigate(['/']);
-    })
+
+    if (this.id !== 0) {
+      // Es editar 
+      product.id = this.id;
+      this._productService.updateProduct(this.id, product).subscribe(() => {
+        this.toastr.info(`El producto ${product.name} fue actualizado con exito`, 'Producto actualizado');
+        this.loading = false;
+        this.router.navigate(['/']);
+      })
+
+    } else {
+      // Es agregagar
+      this._productService.saveProduct(product).subscribe(() => {
+        this.toastr.success(`El producto ${product.name} fue registrado con exito`, 'Producto registrado');
+        this.loading = false;
+        this.router.navigate(['/']);
+      })
+    }
+
+
+
+
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -27,4 +27,12 @@ export class ProductService {
   saveProduct(product: Product): Observable<void> {
     return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`,product)
   }
+
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.myAppUrl}${this.myApiUrl}${id}`)
+  }
+
+  updateProduct(id: number, product: Product): Observable<void> {
+    return this.http.put<void>(`${this.myAppUrl}${this.myApiUrl}${id}`, product);
+  }
 }
